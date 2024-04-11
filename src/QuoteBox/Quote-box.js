dde14d5ad17707db7f46118d7741fe1fb64d47ed
amalmuralii <57037742+amalmuralii@users.noreply.github.com>
2024-04-11 23:45:52 +0530

    Base structure of rqm.

---
+++ src/QuoteBox/Quote-box.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Quote-box.scss';
+
+function QuoteBoxComponent() {
+
+    return (
+        //Do not edit the id names.
+        <div id="quote-box" className="quote-box">
+            <div id="text" className="quote-text">
+            </div>
+            <div id="author" className="author">
+            </div>
+            <a id="tweet-quote"></a>
+        </div>
+    )
+}
+
+export default QuoteBoxComponent
